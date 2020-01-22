0041688404c92557ca149e752c01332f8929832f
jeremie.cuadrado <jeremie.cuadrado@cdiscount.com>
2020-01-22 13:22:55 +0100

    yaml lint fix python fix

--- molecule/default/tests/test_Default.py
+++ molecule/default/tests/test_Default.py
@@ -6,12 +6,14 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 @pytest.mark.parametrize('pkg', [
     'python3'
 ])
 def test_pkg(host, pkg):
     package = host.package(pkg)
     assert package.is_installed
+
 
 def test_file(host):
     file = host.file('/usr/bin/az')
